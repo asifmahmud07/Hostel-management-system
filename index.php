310100285f43847f19f7a5d587cea500af6280bb
asifmahmud07 <awazzee@gmail.com>
2021-01-13 00:18:01 +0600

    UI Design

---
+++ index.php
@@ -0,0 +1,41 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hostel Management</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link href="img/logo.png" type="img/icon" rel="icon">
+</head>
+<body>
+	<div id="full">
+		<div style="background-image: url('img/hostel-1.jpg');">
+			<div id="header">
+				<div id="logo">
+					<h1><font color="white">NSU Hostels</font></h1>
+				</div>
+				<div id="nav">
+					<ul>
+						<li><a href="index.php">Home</a></li>
+						<li><a href="#">About</a></li>
+						<li><a href="#">Contact Us</a></li>
+						
+					</ul>
+				</div>
+			</div>
+			<div id="banner"><br><br><br><br><br><br><br>
+				<center><div style="background-color:rgba(255,255,255,0.9);width:50%;"><br>
+					
+					<center><h2><font color="blue">Sign Up/Login to book rooms</font></h2></center><br>
+				</div></center>
+			</div>
+		</div>
+		<div id="welcome">
+			<h1 align="center"><font color="red">North South University</h1>
+			<center><font size="4">
+				Center of Excellence in Higher Education
+			</font></center>
+		</div>
+	</div>
+
+</body>
+</html>
